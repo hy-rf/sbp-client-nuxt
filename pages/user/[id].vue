83b4305a675a5b9206513d48e2c5210b921fb650
hy-rf <heyu880202@gmail.com>
2025-08-13 00:42:40 +0800

    feat: /user/[id]

---
+++ pages/user/[id].vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+definePageMeta({
+  validate: async (route) => {
+    // Check if the id is made up of digits
+    return typeof route.params.id === 'string' && /^\d+$/.test(route.params.id)
+  }
+})
+
+const route = useRoute();
+const userId = route.params.id as string;
+
+</script>
+
+<template>
+  <div>
+    <p>{{ userId }}</p>
+  </div>
+</template>
+
+<style lang="css" scoped>
+
+</style>
